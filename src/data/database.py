8cdcc40d8e9f49bc16c310447da4e44d45ad24ff
Serhii Bohdanov <serhii.bohdanov@student.tuke.sk>
2023-06-03 00:04:16 +0200

    a simple get endpoint

--- src/data/database.py
+++ src/data/database.py
@@ -6,7 +6,13 @@ from src.data.models import Courier, Food, Order, User
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-engine = create_engine(sqlite_url, echo=True)
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
 
 
 def create_db_and_tables():
