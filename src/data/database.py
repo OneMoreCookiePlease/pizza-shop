8acfd58d8e54f0762db26781409b1bd173fc66ba
Serhii Bohdanov <serhii.bohdanov@student.tuke.sk>
2023-06-02 23:40:06 +0200

    solved relations within models

--- src/data/database.py
+++ src/data/database.py
@@ -1,10 +1,7 @@
-from sqlmodel import SQLModel, create_engine
+from sqlalchemy.orm import relationship
+from sqlmodel import Session, SQLModel, create_engine
 
-from src.data.models.courier import Courier
-from src.data.models.food_item import FoodItem
-from src.data.models.food_order import FoodOrderLink
-from src.data.models.order import Order
-from src.data.models.user import User
+from src.data.models import Courier, Food, Order, User
 
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -15,3 +12,33 @@ engine = create_engine(sqlite_url, echo=True)
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
     print("DB created")
+
+
+def create_mock_data():
+    create_mock_food()
+    create_mock_user()
+    create_mock_courier()
+
+
+def create_mock_food():
+    with Session(engine) as session:
+        meal = Food(name="pizza mozzarella", price=15)
+        session.add(meal)
+        session.commit()
+
+
+def create_mock_user():
+    try:
+        with Session(engine) as session:
+            user = User(name="bob", password_hash="abc")
+            session.add(user)
+            session.commit()
+    except:
+        print("Unique values expected")
+
+
+def create_mock_courier():
+    with Session(engine) as session:
+        courier = Courier(name="billy")
+        session.add(courier)
+        session.commit()
--- src/data/models/courier.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional
-
-from sqlmodel import Field, SQLModel, create_engine
-
-
-class Courier(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    name: str
